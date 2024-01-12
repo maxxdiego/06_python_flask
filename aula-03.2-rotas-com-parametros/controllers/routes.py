8ab59ffcbc1ee99ecc0f98f66189b0afb31d5380
maxx.diego <77diego.max@gmail.com>
2024-01-12 16:22:26 -0300

    @maxxdiego

---
+++ aula-03.2-rotas-com-parametros/controllers/routes.py
@@ -0,0 +1,51 @@
+from flask import render_template, request
+import urllib
+import json
+
+jogadores = []
+gamelist = [{'Título' : 'CS-GO', 'Ano' : 2012, 'Categoria' : 'FPS Online'}]
+
+def init_app(app):
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    @app.route('/games', methods=['GET', 'POST'])
+    def games():
+        game = gamelist[0]
+        
+        if request.method == 'POST':
+            if request.form.get('jogador'):
+                jogadores.append(request.form.get('jogador'))
+        return render_template('games.html',
+                                game=game,
+                                jogadores=jogadores)
+        
+    @app.route('/cadgames', methods=['GET', 'POST'])
+    def cadgames():
+        if request.method == 'POST':
+            if request.form.get('titulo') and request.form.get('ano') and request.form.get('categoria'):
+                gamelist.append({'Título' : request.form.get('titulo'), 'Ano' : request.form.get('ano'), 'Categoria' : request.form.get('categoria')})
+        
+        return render_template('cadgames.html',
+                                gamelist=gamelist)
+    
+    @app.route('/apigames', methods=['GET', 'POST'])
+    @app.route('/apigames/<int:id>', methods=['GET', 'POST'])
+    def apigames(id=None):
+        url = 'https://www.freetogame.com/api/games'
+        res = urllib.request.urlopen(url)
+        data = res.read()
+        gamesjson = json.loads(data)
+        if id:
+            ginfo = []
+            for g in gamesjson:
+                if g['id'] == id:
+                    ginfo=g
+                    break
+            if ginfo:
+                return render_template('gameinfo.html', ginfo=ginfo)
+            else:
+                return f'Game com a ID {id} não foi encontrado.'
+        else:
+            return render_template('apigames.html', gamesjson=gamesjson)
