f7ac593ecbd937ff8fc1877003bc0026679a6fda
maxx.diego <77diego.max@gmail.com>
2024-01-13 16:50:55 -0300

    @maxxdiego

--- aula-01.1-introducao-flask/app.py
+++ aula-01.1-introducao-flask/app.py
@@ -7,4 +7,4 @@ def home(): #Função que será executada ao acessar a rota
     return '<h1>Esta é a Homepage!</h1>' #Retorno que será exibido
 
 if __name__ == '__main__': #SE for executado diretamente pelo interpretador
-    app.run(host='localhost', port=5000, debug=True) #Roda a aplicação em localhost:5000, com o modo debug ativado.
+    app.run(host='localhost', port=5000, debug=True) #Roda a aplicação em localhost:5000, com o modo debug ativado.'
