c56b2fdeee53d2837b39da668de070250cff4a88
maxx.diego <77diego.max@gmail.com>
2024-05-09 12:22:03 -0300

    @maxxdiego

---
+++ aula-09.3-api-mongodb-schemas/api/services/game_service.py
@@ -0,0 +1,13 @@
+from api import mongo
+from ..models import game_model
+
+def add_game(game):
+    mongo.db.games.insert_one({
+        'titulo': game.titulo,
+        'descricao': game.descricao,
+        'ano': game.ano
+    })
+
+@staticmethod
+def get_games():
+    return list(mongo.db.games.find())
---
+++ aula-09.3-api-mongodb-schemas/api/views/games_views.py
@@ -0,0 +1,14 @@
+from flask_restful import Resource
+from api import api
+from flask import make_response, jsonify
+from ..schemas import game_schema
+from ..models import game_model
+from ..services import game_service
+
+class GameList(Resource):
+    def get(self):
+        games = game_service.get_games()
+        g = game_schema.GameSchema(many=True)
+        return make_response(g.jsonify(games), 200)  
+
+api.add_resource(GameList, '/games')
